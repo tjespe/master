2e1ab99a744e650d86f347270635b2cf85a7935c
Tord Johan Espe <tordjohanespe@gmail.com>
2025-02-17 16:03:37 +0100

    Test weight-decayed vbig-fng model

--- Code/settings.py
+++ Code/settings.py
@@ -1,7 +1,7 @@
 # Define parameters
 LOOKBACK_DAYS = 30
 SUFFIX = "_stocks"  # Use "_stocks" for the single stocks or "" for S&P500 only
-TEST_ASSET = "WMT"
+TEST_ASSET = "AON"
 DATA_PATH = "data/sp500_stocks_garch.csv"
 TRAIN_VALIDATION_SPLIT = "2021-12-31"
 VALIDATION_TEST_SPLIT = "2023-12-31"
