ceda297e1111a2ab9cb9f16bdbc481868c4abf97
SEggen <siverteg@gmail.com>
2025-03-16 16:31:09 +0100

    Implemented a R-GARCH model

--- Code/benchmark_models/Benchmark_code/R Scripts/Realized_GARCH_2.R
+++ Code/benchmark_models/Benchmark_code/R Scripts/Realized_GARCH_2.R
@@ -1,7 +1,7 @@
-# Following the Hansen et al. (2012), (2014) papers, we will use the realized GARCH model to forecast volatility.
+# USING THE RUGARCH PACKAGE TO FIT A REALIZED GARCH MODEL TO THE DATA
 
 # Load libraries #
-library(realized) # TURNS OUT THIS PACKAGE IS NOT AVAILABLE ON CRAN, SO IT IS NOT POSSIBLE TO INSTALL IT
+library(rugarch)
 library(readr)
 
 # Load data #
@@ -12,7 +12,7 @@ return_data = read.csv("~/Masterv3/master/Code/data/dow_jones/processed_data/dow
 
 # Return data
 # Remove all coloumns except Date, Symbol, Total Return and LogReturn
-return_data = return_data[,c("Date", "Symbol", "TotalReturn", "LogReturn")]
+return_data = return_data[,c("Date", "Symbol", "Total.Return", "LogReturn")]
 # remove .O at the end of all symbols
 return_data$Symbol = gsub("\\.O", "", return_data$Symbol)
 # ensure that the Date column is in the correct format
@@ -27,16 +27,19 @@ capire_data$Date = as.Date(capire_data$Date, format = "%Y-%m-%d")
 capire_data$RV_5 = (capire_data$RV_5/100)/252
 
 # sort data by Date and Symbol
-return_data = return_data[order(return_data$Date, return_data$Symbol),]
-capire_data = capire_data[order(capire_data$Date, capire_data$Symbol),]
+return_data = return_data[order(return_data$Symbol, return_data$Date),]
+capire_data = capire_data[order(capire_data$Symbol, capire_data$Date),]
 
 # Merge data on Date and Symbol
 data = merge(return_data, capire_data, by = c("Date", "Symbol"))
+data = data[order(data$Symbol, data$Date),]
 
 # define training and validation data
 training_data = data[data$Date < as.Date("2021-12-31"),]
 validation_data = data[data$Date >= as.Date("2021-12-31") & data$Date <= as.Date("2023-12-31"),]
 
+
+############################################
 # Loop trough symbols and fit model
 symbols = unique(data$Symbol)
 
@@ -61,36 +64,62 @@ for (symbol in symbols) {
     dates = as.Date(symbol_data$Date)
 
     total_observations = length(returns)
-    forecast_volatility <- rep(NA, total_obs - initial_window_size)
-    forecast_dates <- dates[(initial_window_size + 1):total_obs] 
+    forecast_volatility <- rep(NA, total_observations - window_size)
+    forecast_dates <- dates[(window_size + 1):total_observations] 
 
-    for (i in seq(initial_window_size, total_obs - 1)) {
+    for (i in seq(window_size, total_observations - 1)) {
         # define expanding window
         returns_train = returns[1:i]
-        rv_train = rv[1:i]
+        RV_train = rv[1:i]
 
-        # fit realized garch model
-        rgarch_model <- realizedGARCH(r = returns_train, realized = RV_train)
-        rgarch_forecast <- predict(rgarch_model, n.ahead = 1)
+        # external regressors
+        logRV_train_lagged <- log(RV_train[-length(RV_train)])
+        returns_train_trimmed <- returns_train[-1]  # Align
 
-        # store forecasted volatility
-        forecast_volatility[i - initial_window_size + 1] <- rgarch_forecast$vol
+        # Model specification for each iteration
+        spec <- ugarchspec(
+            variance.model = list(
+            model = "sGARCH",
+            garchOrder = c(1, 1),
+            external.regressors = matrix(logRV_train_lagged, ncol = 1)
+            ),
+            mean.model = list(
+            armaOrder = c(0, 0),
+            include.mean = TRUE
+            ),
+            distribution.model = "norm"
+        )
+
+        # Fit the model on current expanding window
+        fit <- ugarchfit(spec = spec, data = returns_train_trimmed, solver = "hybrid")
+
+        # Prepare external regressor for the next period forecast
+        # Using log RV of the current last training observation
+        logRV_forecast <- log(RV_train[length(RV_train)])
+        
+        # Forecast 1-step ahead
+        forecast <- ugarchforecast(fit, n.ahead = 1,
+                                    external.forecasts = list(vexdata = logRV_forecast))
+        
+        # Extract the 1-step-ahead conditional variance forecast (sigma^2)
+        forecast_volatility[i - window_size + 1] <- sigma(forecast)
 
         # Progress indicator
-        if ((i - initial_window_size + 1) %% 10 == 0) {
-            print(paste0("Progress: ", round(100 * (i - initial_window_size + 1) / (total_obs - initial_window_size), 2), "%"))
+        if ((i - window_size + 1) %% 10 == 0) {
+            print(paste0("Progress: ", round(100 * (i - window_size + 1) / (total_observations - window_size), 2), "%"))
         }
     }
 
 
     # store results
     # === Combine results into a forecast data frame ===
+    # Create forecast results data frame
     forecast_results <- data.frame(
     Date = forecast_dates,
+    Symbol = symbol,
     Forecast_Volatility = forecast_volatility,
-    Forecast_Realized = forecast_realized,
     Mean = 0
-)
+    )
 
     results[[symbol]] = forecast_results
 }
