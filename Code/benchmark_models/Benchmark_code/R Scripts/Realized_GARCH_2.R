44ae4b369624a2accecbad96ed92a93b2b2b36fb
SEggen <siverteg@gmail.com>
2025-03-17 11:15:02 +0100

    R-GARCH implementation RV as exogenous

--- Code/benchmark_models/Benchmark_code/R Scripts/Realized_GARCH_2.R
+++ Code/benchmark_models/Benchmark_code/R Scripts/Realized_GARCH_2.R
@@ -1,18 +1,22 @@
 # USING THE RUGARCH PACKAGE TO FIT A REALIZED GARCH MODEL TO THE DATA
-
 # Load libraries #
 library(rugarch)
 library(readr)
+library(data.table)
 
 # Load data #
-capire_data = read.csv("~/Masterv3/master/Code/data/dow_jones/processed_data/processed_capire_stock_data_dow_jones.csv")
-return_data = read.csv("~/Masterv3/master/Code/data/dow_jones/processed_data/dow_jones_stocks_1990_to_today_19022025_cleaned_garch.csv")
+capire_data <- read.csv("~/Masterv3/master/Code/data/dow_jones/processed_data/processed_capire_stock_data_dow_jones.csv")
+return_data <- read.csv("~/Masterv3/master/Code/data/dow_jones/processed_data/dow_jones_stocks_1990_to_today_19022025_cleaned_garch.csv")
+
+# define what distribution assumption we would like to use
+dist_assumption <- "norm"  # set "norm" for normal and "std" for student-t
+
 
 # Clean data #
 
 # Return data
 # Remove all coloumns except Date, Symbol, Total Return and LogReturn
-return_data = return_data[,c("Date", "Symbol", "Total.Return", "LogReturn")]
+return_data <- return_data[,c("Date", "Symbol", "Total.Return", "LogReturn")]
 # remove .O at the end of all symbols
 return_data$Symbol = gsub("\\.O", "", return_data$Symbol)
 # ensure that the Date column is in the correct format
@@ -20,57 +24,79 @@ return_data$Date = as.Date(return_data$Date, format = "%Y-%m-%d")
 
 # Capire data/RV data
 # Remove all coloumns except Date, Symbol and RV_5
-capire_data = capire_data[,c("Date", "Symbol", "RV_5")]
+capire_data <- capire_data[,c("Date", "Symbol", "RV_5")]
 # ensure that the Date column is in the correct format
 capire_data$Date = as.Date(capire_data$Date, format = "%Y-%m-%d")
 # transform the RV to become daily_rv
 capire_data$RV_5 = (capire_data$RV_5/100)/252
 
 # sort data by Date and Symbol
-return_data = return_data[order(return_data$Symbol, return_data$Date),]
-capire_data = capire_data[order(capire_data$Symbol, capire_data$Date),]
+return_data <- return_data[order(return_data$Symbol, return_data$Date),]
+capire_data <- capire_data[order(capire_data$Symbol, capire_data$Date),]
 
 # Merge data on Date and Symbol
-data = merge(return_data, capire_data, by = c("Date", "Symbol"))
-data = data[order(data$Symbol, data$Date),]
+data <- merge(return_data, capire_data, by = c("Date", "Symbol"))
+data <- data[order(data$Symbol, data$Date),]
+
+# remove rows with NA values
+data <- data[complete.cases(data),]
+
 
 # define training and validation data
-training_data = data[data$Date < as.Date("2021-12-31"),]
-validation_data = data[data$Date >= as.Date("2021-12-31") & data$Date <= as.Date("2023-12-31"),]
+training_data <- data[data$Date < as.Date("2021-12-31"),]
+validation_data <- data[data$Date >= as.Date("2021-12-31") & data$Date <= as.Date("2023-12-31"),]
 
 
 ############################################
 # Loop trough symbols and fit model
-symbols = unique(data$Symbol)
+symbols <- unique(data$Symbol)
+
+# remove all sybols except TRV, UNH, V, VZ, WMT
+symbols <- symbols[symbols %in% c("TRV", "UNH", "V", "VZ", "WMT")]
+
+
+for (symbol in symbols) {
+  symbol_data <- data[data$Symbol == symbol,]
+  rv_values <- symbol_data$RV_5
+  
+  # Percent of zeros
+  percent_zeros <- sum(rv_values == 0) / length(rv_values)
+  
+  cat(paste0("Symbol: ", symbol, " | Percent Zeros in RV: ", round(percent_zeros * 100, 2), "%\n"))
+}
+
 
 # inititialize list to store results
-results = list()
+results <- list()
 
 for (symbol in symbols) {
     print(paste0("Fitting model for symbol: ", symbol))
     # filter data for symbol
-    symbol_data = data[data$Symbol == symbol,]
-    symbol_training_data = training_data[training_data$Symbol == symbol,]
-    symbol_validation_data = validation_data[validation_data$Symbol == symbol,]
+    symbol_data <- data[data$Symbol == symbol,]
+    symbol_training_data <- training_data[training_data$Symbol == symbol,]
+    symbol_validation_data <- validation_data[validation_data$Symbol == symbol,]
     
     # define initial window size
-    window_size = length(symbol_training_data$Date)
+    window_size <- length(symbol_training_data$Date)
 
     # concatenate training and validation data
-    symbol_data = rbind(symbol_training_data, symbol_validation_data)
+    symbol_data <- rbind(symbol_training_data, symbol_validation_data)
 
-    returns = as.numeric(symbol_data$LogReturn)
-    rv = as.numeric(symbol_data$RV_5)
-    dates = as.Date(symbol_data$Date)
+    returns <- as.numeric(symbol_data$LogReturn)
+    rv <- as.numeric(symbol_data$RV_5)
+    #if there are 0 values, replace with very small number 
+    epsilon <- 1e-8
+    rv <- pmax(rv, epsilon)
+    dates <- as.Date(symbol_data$Date)
 
-    total_observations = length(returns)
+    total_observations <- length(returns)
     forecast_volatility <- rep(NA, total_observations - window_size)
     forecast_dates <- dates[(window_size + 1):total_observations] 
 
     for (i in seq(window_size, total_observations - 1)) {
         # define expanding window
-        returns_train = returns[1:i]
-        RV_train = rv[1:i]
+        returns_train <- returns[1:i]
+        RV_train <- rv[1:i]
 
         # external regressors
         logRV_train_lagged <- log(RV_train[-length(RV_train)])
@@ -87,7 +113,7 @@ for (symbol in symbols) {
             armaOrder = c(0, 0),
             include.mean = TRUE
             ),
-            distribution.model = "norm"
+            distribution.model = dist_assumption
         )
 
         # Fit the model on current expanding window
@@ -124,7 +150,10 @@ for (symbol in symbols) {
     results[[symbol]] = forecast_results
 }
 
+final_results <- rbindlist(results)
+
 # write results to csv
-write.csv(results, "~/Masterv3/master/Code/predictions/realized_garch_forecast.csv")
+write.csv(final_results, paste0("~/Masterv3/master/Code/predictions/realized_garch_forecast_", dist_assumption, ".csv"))
+
 
 
