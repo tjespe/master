65a07cdfa64c2ef5e047c33002a376df648b40c6
SEggen <siverteg@gmail.com>
2025-03-24 10:43:35 +0100

    made Garch student-t model

--- Code/compare_models_multi_asset.py
+++ Code/compare_models_multi_asset.py
@@ -143,6 +143,56 @@ for garch_type in ["GARCH", "EGARCH"]:
     except FileNotFoundError:
         print(f"{garch_type} predictions not found")
 
+# GARCH Student-t Model
+try:
+    garch_t_vol_pred = pd.read_csv("predictions/garch_predictions_student_t.csv")
+    garch_t_vol_pred["Date"] = pd.to_datetime(garch_t_vol_pred["Date"])
+    garch_t_vol_pred = garch_t_vol_pred.set_index(["Date", "Symbol"])
+    garch_t_dates = garch_t_vol_pred.index.get_level_values("Date")
+    garch_t_vol_pred = garch_t_vol_pred[
+        (garch_t_dates >= TRAIN_VALIDATION_SPLIT) & (garch_t_dates < VALIDATION_TEST_SPLIT)
+    ]
+    combined_df = df_validation.join(garch_t_vol_pred, how="left", rsuffix="_GARCH_t")
+    garch_t_vol_pred = combined_df["GARCH_t_Vol"].values
+    y_true = combined_df["LogReturn"].values
+    mus = np.zeros_like(garch_t_vol_pred)
+
+    entry = {
+        "name": "GARCH Student-t",
+        "mean_pred": mus,
+        "volatility_pred": garch_t_vol_pred,
+        "symbols": combined_df.index.get_level_values("Symbol"),
+        "nll": nll_loss_mean_and_vol(
+            y_true,
+            mus,
+            garch_t_vol_pred,
+        ),
+        "crps": crps_normal_univariate(y_true, mus, garch_t_vol_pred),
+    }
+
+    for cl in CONFIDENCE_LEVELS:
+        alpha = 1 - cl
+        z_alpha = norm.ppf(1 - alpha / 2)
+        lb = mus - z_alpha * garch_t_vol_pred
+        ub = mus + z_alpha * garch_t_vol_pred
+        entry[f"LB_{format_cl(cl)}"] = lb
+        entry[f"UB_{format_cl(cl)}"] = ub
+        es_alpha = alpha / 2
+        entry[f"ES_{format_cl(1-es_alpha)}"] = calculate_es_for_quantile(
+            np.ones_like(mus).reshape(-1, 1),
+            mus.reshape(-1, 1),
+            garch_t_vol_pred.reshape(-1, 1),
+            lb,
+        )
+
+    preds_per_model.append(entry)
+    nans = np.isnan(garch_t_vol_pred).sum()
+    if nans > 0:
+        print(f"GARCH Student-t has {nans} NaN predictions")
+except FileNotFoundError:
+    print("GARCH Student-t predictions not found")
+    
+
 # HAR Model
 for version in ["python", "R"]:
     try:
