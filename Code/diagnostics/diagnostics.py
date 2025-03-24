b9d8cadc2ef35c7ba622997e51ecd1495289bf27
SEggen <siverteg@gmail.com>
2025-03-24 14:40:17 +0100

    Librarian

--- Code/diagnostics/diagnostics.py
+++ Code/diagnostics/diagnostics.py
@@ -8,3 +8,18 @@
 # 4. Durbin-Watson statistic for residual autocorrelation (test on each stock)
 # 5. Jarque-Bera test for normality (test on each stock)
 # 6 VIF (Variance Inflation Factor) for multicollinearity (test on each stock)
+
+
+# %%
+# import the necessary libraries
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.stats.diagnostic import acorr_ljungbox
+from statsmodels.stats.diagnostic import het_arch
+from statsmodels.stats.stattools import durbin_watson
+from statsmodels.stats.stattools import jarque_bera
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from statsmodels.stats.outliers_influence import OLSInfluence
+from statsmodels.stats.outliers_influence import reset_ramsey
