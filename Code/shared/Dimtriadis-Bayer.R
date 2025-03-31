6ad611e3702adbb50bca9fbe0bb2c4bc04d7e816
SEggen <siverteg@gmail.com>
2025-03-31 10:35:17 +0200

    Changes

--- Code/shared/Dimtriadis-Bayer.R
+++ Code/shared/Dimtriadis-Bayer.R
@@ -8,7 +8,7 @@ library(esback)
 
 ############ LSTM ###################
 
-
+lstm_IV_ensemble <- mydata <- read.csv("../predictions/lstm_mdn_predictions_stocks_vivol-final_ensemble.csv")
 ########## GARCH ###########
 
 
@@ -16,3 +16,23 @@ library(esback)
 
 
 ########## DB ###########
+
+
+
+
+# implement the test
+cov_settings <- list(
+    sparsity = "nid",
+    sigma_est = "scl_sp",
+  misspec = TRUE           # Allow for model misspecification (robust covariance)
+)
+
+# version specification: 1 = Strict ESR, 2 = Auxilliary ESR, 3 = Strict Interceots
+result <- esr_backtest(
+  r = r,
+  q = q,
+  e = e,
+  alpha = alpha,
+  version = 1,
+  cov_config = cov_settings
+)
