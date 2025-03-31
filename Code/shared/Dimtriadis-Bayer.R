df6c23d4bc756073bc4ad2b33d547db686668be8
SEggen <siverteg@gmail.com>
2025-03-31 10:29:04 +0200

    Added an R file for implementing the Strict Dimitriadis Backtest

---
+++ Code/shared/Dimtriadis-Bayer.R
@@ -0,0 +1,18 @@
+install.packages("esback")  # Only once
+library(esback)
+
+# load predictions from different models as a data frame
+
+############# Transformer #############
+
+
+############ LSTM ###################
+
+
+########## GARCH ###########
+
+
+########### BOOSTERS ###########
+
+
+########## DB ###########
