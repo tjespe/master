82a4b788ac1d1eb6a3e91d1950fa40b340782a45
Tord Johan Espe <tordjohanespe@gmail.com>
2025-04-01 15:21:03 +0200

    Update

--- Code/settings.py
+++ Code/settings.py
@@ -7,8 +7,8 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 LOOKBACK_DAYS = 30
 SUFFIX = "_stocks"  # Use "_stocks" for the single stocks or "" for S&P500 only
 DATA_PATH = f"{BASEDIR}/data/dow_jones/processed_data/dow_jones_stocks_1990_to_today_19022025_cleaned_garch.csv"
-TRAIN_VALIDATION_SPLIT = "2021-12-31"
-VALIDATION_TEST_SPLIT = "2022-12-31"
+TRAIN_VALIDATION_SPLIT = "2018-12-31"
+VALIDATION_TEST_SPLIT = "2019-12-31"
 
 # Only used in old single stock model comparison
 TEST_ASSET = "AAPL"
