9c84bcb2bf0c02aa31dadb3f1388c8f3c26f3f8f
Tord Johan Espe <tordjohanespe@gmail.com>
2025-04-23 10:40:48 +0200

    Adjust limit

--- Code/transformer_mdn_ensemble_expanding.py
+++ Code/transformer_mdn_ensemble_expanding.py
@@ -196,7 +196,7 @@ if __name__ == "__main__":
     dates = []
     true_y = []
     while (test := data.get_test_set_for_date(first_test_date, end_date())).y.shape[0]:
-        if first_test_date < pd.to_datetime(date(2023, 1, 1)):
+        if first_test_date < pd.to_datetime(date(2022, 10, 1)):
             # Skip this for now because it runs on another machine
             print("Skipping", first_test_date.date().isoformat())
             first_test_date = end_date() + pd.DateOffset(days=1)
