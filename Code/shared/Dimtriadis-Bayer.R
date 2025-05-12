c2e75cdaa3e920a8c17fd02a4fd48aebd2fac849
Tord Johan Espe <tordjohanespe@gmail.com>
2025-05-12 16:17:51 +0200

    Remove all lstm preds in case they are wrong

--- Code/shared/Dimtriadis-Bayer.R
+++ Code/shared/Dimtriadis-Bayer.R
@@ -12,9 +12,8 @@ test_set_start_date <- "2019-12-31"
 
 # get return data for the test set
 return_data <- read.csv(file.path(base_path_return_data, "dow_jones_stocks_1990_to_today_19022025_cleaned_garch.csv"))
-# filter only for test set, fr
-om 2023-01-03 to 2024-03-28
-return_data <- return_data[return_data$Date >= test_set_start_date & return_data$Date <= "2024-03-28", ] 
+# filter only for test set, from 2023-01-03 to 2024-03-28
+return_data <- return_data[return_data$Date >= test_set_start_date & return_data$Date <= "2024-03-28", ]
 # return_data <- return_data[return_data$Date >= "2005-02-15" & return_data$Date <= "2024-03-28", ]
 # remove .O at the end of the Symbol for the return data
 return_data$Symbol <- gsub("\\.O$", "", return_data$Symbol)
@@ -27,40 +26,40 @@ return_data$Symbol <- gsub("\\.O$", "", return_data$Symbol)
 # %%
 
 # non-rolling
-transformer_RV_ensemble     <- read.csv(file.path(base_path_predictions, "transformer_mdn_predictions_stocks_vrv_test_ensemble.csv"))
-transformer_IV_ensemble     <- read.csv(file.path(base_path_predictions, "transformer_mdn_predictions_stocks_vivol_test_ensemble.csv"))
-transformer_RV_IV_ensemble  <- read.csv(file.path(base_path_predictions, "transformer_mdn_predictions_stocks_vrv-and-ivol_test_ensemble.csv"))
+transformer_RV_ensemble <- read.csv(file.path(base_path_predictions, "transformer_mdn_predictions_stocks_vrv_test_ensemble.csv"))
+transformer_IV_ensemble <- read.csv(file.path(base_path_predictions, "transformer_mdn_predictions_stocks_vivol_test_ensemble.csv"))
+transformer_RV_IV_ensemble <- read.csv(file.path(base_path_predictions, "transformer_mdn_predictions_stocks_vrv-and-ivol_test_ensemble.csv"))
 
 # rolling
-transformer_RV_ensemble_rolling     <- read.csv(file.path(base_path_predictions, "transformer_mdn_ensemble_rvol_test_expanding.csv"))
-transformer_IV_ensemble_rolling     <- read.csv(file.path(base_path_predictions, "transformer_mdn_ensemble_ivol_test_expanding.csv"))
-transformer_RV_IV_ensemble_rolling  <- read.csv(file.path(base_path_predictions, "transformer_mdn_ensemble_rvol-ivol_test_expanding.csv"))
+transformer_RV_ensemble_rolling <- read.csv(file.path(base_path_predictions, "transformer_mdn_ensemble_rvol_test_expanding.csv"))
+transformer_IV_ensemble_rolling <- read.csv(file.path(base_path_predictions, "transformer_mdn_ensemble_ivol_test_expanding.csv"))
+transformer_RV_IV_ensemble_rolling <- read.csv(file.path(base_path_predictions, "transformer_mdn_ensemble_rvol-ivol_test_expanding.csv"))
 
 ############ LSTM ###################
 # %%
 
-#non-rolling
-lstm_RV_ensemble     <- read.csv(file.path(base_path_predictions, "lstm_mdn_predictions_stocks_vrv-final_ensemble.csv"))
-lstm_IV_ensemble     <- read.csv(file.path(base_path_predictions, "lstm_mdn_predictions_stocks_vivol-final_ensemble.csv"))
-lstm_RV_IV_ensemble  <- read.csv(file.path(base_path_predictions, "lstm_mdn_predictions_stocks_vrv-and-ivol-final_ensemble.csv"))
+# non-rolling
+# lstm_RV_ensemble <- read.csv(file.path(base_path_predictions, "lstm_mdn_predictions_stocks_vrv-final_ensemble.csv"))
+# lstm_IV_ensemble <- read.csv(file.path(base_path_predictions, "lstm_mdn_predictions_stocks_vivol-final_ensemble.csv"))
+# lstm_RV_IV_ensemble <- read.csv(file.path(base_path_predictions, "lstm_mdn_predictions_stocks_vrv-and-ivol-final_ensemble.csv"))
 
-#rolling
-lstm_RV_ensemble_rolling     <- read.csv(file.path(base_path_predictions, "lstm_mdn_ensemble_stocks_vrv-final-rolling_test.csv"))
-lstm_IV_ensemble_rolling     <- read.csv(file.path(base_path_predictions, "lstm_mdn_ensemble_stocks_vivol-final-rolling_test.csv"))
-lstm_RV_IV_ensemble_rolling  <- read.csv(file.path(base_path_predictions, "lstm_mdn_ensemble_stocks_vrv-and-ivol-final-rolling_test.csv"))
+# rolling
+lstm_RV_ensemble_rolling <- read.csv(file.path(base_path_predictions, "lstm_mdn_ensemble_stocks_vrv-final-rolling_test.csv"))
+lstm_IV_ensemble_rolling <- read.csv(file.path(base_path_predictions, "lstm_mdn_ensemble_stocks_vivol-final-rolling_test.csv"))
+lstm_RV_IV_ensemble_rolling <- read.csv(file.path(base_path_predictions, "lstm_mdn_ensemble_stocks_vrv-and-ivol-final-rolling_test.csv"))
 
 ############## ENSEMBLE COMBINATIONS ###############
-#MDN_ensemble_IV_RV <- read.csv(file.path(base_path_predictions, "ensemble_mdn_predictions_stocks_vrv-iv_ensemble.csv"))
+# MDN_ensemble_IV_RV <- read.csv(file.path(base_path_predictions, "ensemble_mdn_predictions_stocks_vrv-iv_ensemble.csv"))
 
 ########## GARCH MODELS ###########
 # %%
-garch_norm     <- read.csv(file.path(base_path_predictions, "GARCH_preds_enriched.csv"))
-garch_t        <- read.csv(file.path(base_path_predictions, "garch_predictions_student_t.csv"))
-garch_skew_t        <- read.csv(file.path(base_path_predictions, "garch_predictions_skewed_t.csv"))
-rv_garch       <- read.csv(file.path(base_path_predictions, "realized_garch_forecast_std.csv"))
-ar_garch_norm  <- read.csv(file.path(base_path_predictions, "predictions_AR(1)-GARCH(1,1)-normal.csv"))
-ar_garch_t   <- read.csv(file.path(base_path_predictions, "predictions_AR(1)-GARCH(1,1)-t.csv"))  # Not included because file name is missing
-egarch         <- read.csv(file.path(base_path_predictions, "EGARCH_preds_enriched.csv"))
+garch_norm <- read.csv(file.path(base_path_predictions, "GARCH_preds_enriched.csv"))
+garch_t <- read.csv(file.path(base_path_predictions, "garch_predictions_student_t.csv"))
+garch_skew_t <- read.csv(file.path(base_path_predictions, "garch_predictions_skewed_t.csv"))
+rv_garch <- read.csv(file.path(base_path_predictions, "realized_garch_forecast_std.csv"))
+ar_garch_norm <- read.csv(file.path(base_path_predictions, "predictions_AR(1)-GARCH(1,1)-normal.csv"))
+ar_garch_t <- read.csv(file.path(base_path_predictions, "predictions_AR(1)-GARCH(1,1)-t.csv")) # Not included because file name is missing
+egarch <- read.csv(file.path(base_path_predictions, "EGARCH_preds_enriched.csv"))
 
 
 # filter only for test set, from test_set_start_date to 2024-03-28
@@ -84,35 +83,35 @@ egarch$Symbol <- gsub("\\.O$", "", egarch$Symbol)
 
 ######### HAR ##############
 # %%
-har   <- read.csv(file.path(base_path_predictions, "HAR_python.csv"))
-harq  <- read.csv(file.path(base_path_predictions, "HARQ_python.csv"))
-har_qreq  <- read.csv(file.path(base_path_predictions, "HAR_qreg_test.csv"))
-har_ivol_qreq  <- read.csv(file.path(base_path_predictions, "HAR_IVOL_qreg_test.csv"))
+har <- read.csv(file.path(base_path_predictions, "HAR_python.csv"))
+harq <- read.csv(file.path(base_path_predictions, "HARQ_python.csv"))
+har_qreq <- read.csv(file.path(base_path_predictions, "HAR_qreg_test.csv"))
+har_ivol_qreq <- read.csv(file.path(base_path_predictions, "HAR_IVOL_qreg_test.csv"))
 
 ########### BOOSTERS ###########
 # %%
 catboost_RV <- read.csv(file.path(base_path_predictions, "CatBoost_RV_4y.csv"))
-catboost_IV     <- read.csv(file.path(base_path_predictions, "CatBoost_IV_4y.csv"))
-catboost_RV_IV  <- read.csv(file.path(base_path_predictions, "CatBoost_RV_IV_4y.csv"))
+catboost_IV <- read.csv(file.path(base_path_predictions, "CatBoost_IV_4y.csv"))
+catboost_RV_IV <- read.csv(file.path(base_path_predictions, "CatBoost_RV_IV_4y.csv"))
 
-xgboost_RV      <- read.csv(file.path(base_path_predictions, "XGBoost_RV_4y.csv"))
-xgboost_IV    <- read.csv(file.path(base_path_predictions, "XGBoost_IV_4y.csv"))
+xgboost_RV <- read.csv(file.path(base_path_predictions, "XGBoost_RV_4y.csv"))
+xgboost_IV <- read.csv(file.path(base_path_predictions, "XGBoost_IV_4y.csv"))
 xgboost_RV_IV <- read.csv(file.path(base_path_predictions, "XGBoost_RV_IV_4y.csv"))
 
-lightgbm_RV     <- read.csv(file.path(base_path_predictions, "LightGBM_RV_4y.csv"))
-lightgbm_IV     <- read.csv(file.path(base_path_predictions, "LightGBM_IV_4y.csv"))
-lightgbm_RV_IV  <- read.csv(file.path(base_path_predictions, "LightGBM_RV_IV_4y.csv"))
+lightgbm_RV <- read.csv(file.path(base_path_predictions, "LightGBM_RV_4y.csv"))
+lightgbm_IV <- read.csv(file.path(base_path_predictions, "LightGBM_IV_4y.csv"))
+lightgbm_RV_IV <- read.csv(file.path(base_path_predictions, "LightGBM_RV_IV_4y.csv"))
 
 ########## DB ###########
 # %%
-DB_RV     <- read.csv(file.path(base_path_predictions, "DB_RV.csv"))
-DB_IV     <- read.csv(file.path(base_path_predictions, "DB_IV.csv"))
-DB_RV_IV  <- read.csv(file.path(base_path_predictions, "DB_RV_IV.csv"), check.names = FALSE)
+DB_RV <- read.csv(file.path(base_path_predictions, "DB_RV.csv"))
+DB_IV <- read.csv(file.path(base_path_predictions, "DB_IV.csv"))
+DB_RV_IV <- read.csv(file.path(base_path_predictions, "DB_RV_IV.csv"), check.names = FALSE)
 
 # Filter away dates before test_set_start_date
-DB_RV     <- DB_RV[DB_RV$Date >= test_set_start_date, ]
-DB_IV     <- DB_IV[DB_IV$Date >= test_set_start_date, ]
-DB_RV_IV  <- DB_RV_IV[DB_RV_IV$Date >= test_set_start_date, ]
+DB_RV <- DB_RV[DB_RV$Date >= test_set_start_date, ]
+DB_IV <- DB_IV[DB_IV$Date >= test_set_start_date, ]
+DB_RV_IV <- DB_RV_IV[DB_RV_IV$Date >= test_set_start_date, ]
 
 
 # %%
@@ -123,9 +122,9 @@ transformer_RV_IV_ensemble <- transformer_RV_IV_ensemble[transformer_RV_IV_ensem
 transformer_RV_ensemble_rolling <- transformer_RV_ensemble_rolling[transformer_RV_ensemble_rolling$Symbol != "DOW", ]
 transformer_IV_ensemble_rolling <- transformer_IV_ensemble_rolling[transformer_IV_ensemble_rolling$Symbol != "DOW", ]
 transformer_RV_IV_ensemble_rolling <- transformer_RV_IV_ensemble_rolling[transformer_RV_IV_ensemble_rolling$Symbol != "DOW", ]
-lstm_RV_ensemble <- lstm_RV_ensemble[lstm_RV_ensemble$Symbol != "DOW", ]
-lstm_IV_ensemble <- lstm_IV_ensemble[lstm_IV_ensemble$Symbol != "DOW", ]
-lstm_RV_IV_ensemble <- lstm_RV_IV_ensemble[lstm_RV_IV_ensemble$Symbol != "DOW", ]
+# lstm_RV_ensemble <- lstm_RV_ensemble[lstm_RV_ensemble$Symbol != "DOW", ]
+# lstm_IV_ensemble <- lstm_IV_ensemble[lstm_IV_ensemble$Symbol != "DOW", ]
+# lstm_RV_IV_ensemble <- lstm_RV_IV_ensemble[lstm_RV_IV_ensemble$Symbol != "DOW", ]
 lstm_RV_ensemble_rolling <- lstm_RV_ensemble_rolling[lstm_RV_ensemble_rolling$Symbol != "DOW", ]
 lstm_IV_ensemble_rolling <- lstm_IV_ensemble_rolling[lstm_IV_ensemble_rolling$Symbol != "DOW", ]
 lstm_RV_IV_ensemble_rolling <- lstm_RV_IV_ensemble_rolling[lstm_RV_IV_ensemble_rolling$Symbol != "DOW", ]
@@ -154,7 +153,7 @@ ar_garch_norm <- ar_garch_norm[ar_garch_norm$Symbol != "DOW", ]
 ar_garch_t <- ar_garch_t[ar_garch_t$Symbol != "DOW", ]
 egarch <- egarch[egarch$Symbol != "DOW", ]
 #######################################################################################################
- # TEST MODELS
+# TEST MODELS
 #######################################################################################################
 
 
@@ -249,7 +248,7 @@ es_config_db_rv_iv <- list(
 
 # Model lists
 model_list_DB_RV <- list(
- "DB_RV" = DB_RV
+  "DB_RV" = DB_RV
 )
 model_list_DB_IV <- list(
   "DB_IV" = DB_IV
@@ -291,17 +290,17 @@ head(transformer_RV_ensemble_rolling[, grep("^ES", names(transformer_RV_ensemble
 # %%
 run_esr_backtests <- function(all_model_groups, return_data, test_versions = c(1), sig_levels = c(0.05)) {
   all_results <- list()
-  
+
   for (test_version in test_versions) {
     for (sig in sig_levels) {
       cat("Running ESR backtest for test version:", test_version, "and significance level:", sig, "\n")
       all_combined <- data.frame()
-      
+
       for (group in all_model_groups) {
         model_list <- group$models
         alpha_config <- group$alpha_config
         es_config <- group$es_config
-        
+
         for (model_name in names(model_list)) {
           model_data <- model_list[[model_name]]
           symbols <- unique(model_data$Symbol)
@@ -313,9 +312,9 @@ run_esr_backtests <- function(all_model_groups, return_data, test_versions = c(1
             pass_count <- 0
             fail_count <- 0
 
-            alpha_col <- alpha_config$columns[[ as.character(alpha) ]]
-            es_col    <- es_config$columns[[ as.character(alpha) ]]
-            
+            alpha_col <- alpha_config$columns[[as.character(alpha)]]
+            es_col <- es_config$columns[[as.character(alpha)]]
+
             for (sym in symbols) {
               # subset and align by Date
               returns <- return_data %>%
@@ -323,31 +322,36 @@ run_esr_backtests <- function(all_model_groups, return_data, test_versions = c(1
                 select(Date, LogReturn) %>%
                 arrange(Date)
 
-              preds   <- model_data %>%
+              preds <- model_data %>%
                 filter(Symbol == sym) %>%
                 select(Date, all_of(c(alpha_col, es_col))) %>%
                 arrange(Date)
-              
+
               # do an inner???join to line up dates exactly
-              joint   <- inner_join(returns, preds, by = "Date")
-              if (nrow(joint) == 0) next  # no overlap
-              
+              joint <- inner_join(returns, preds, by = "Date")
+              if (nrow(joint) == 0) next # no overlap
+
               # now extract r, q, e
               r <- joint$LogReturn
               q <- joint[[alpha_col]]
               e <- joint[[es_col]]
-              
-              result <- tryCatch({
-                esr_backtest(
-                  r = r,
-                  q = q,
-                  e = e,
-                  alpha = alpha,
-                  version = test_version,
-                  cov_config = list(sparsity = "nid", sigma_est = "scl_sp", misspec = TRUE)
-                )
-              }, error = function(e) return(NULL))
-              
+
+              result <- tryCatch(
+                {
+                  esr_backtest(
+                    r = r,
+                    q = q,
+                    e = e,
+                    alpha = alpha,
+                    version = test_version,
+                    cov_config = list(sparsity = "nid", sigma_est = "scl_sp", misspec = TRUE)
+                  )
+                },
+                error = function(e) {
+                  return(NULL)
+                }
+              )
+
               if (!is.null(result)) {
                 pval <- result$pvalue_twosided_asymptotic
                 if (!is.null(pval)) {
@@ -355,10 +359,10 @@ run_esr_backtests <- function(all_model_groups, return_data, test_versions = c(1
                 }
               }
             }
-            
+
             total <- pass_count + fail_count
             fail_rate <- ifelse(total > 0, fail_count / total, NA)
-            
+
             all_combined <- rbind(all_combined, data.frame(
               Model = model_name,
               Alpha = alpha,
@@ -372,12 +376,12 @@ run_esr_backtests <- function(all_model_groups, return_data, test_versions = c(1
           }
         }
       }
-      
+
       key <- paste0("Version_", test_version, "_Sig_", sig)
       all_results[[key]] <- all_combined
     }
   }
-  
+
   return(all_results)
 }
 
@@ -411,7 +415,7 @@ for (key in names(esr_results)) {
   cat("\n### Results for", key, "\n")
   df <- esr_results[[key]]
   df$Alpha <- factor(df$Alpha, levels = c(0.01, 0.025, 0.05), labels = c("1%", "2.5%", "5%"))
-  print(kable(df, format = "pipe", digits = 2, align = 'c'))
+  print(kable(df, format = "pipe", digits = 2, align = "c"))
 }
 
 
@@ -425,16 +429,16 @@ for (key in names(esr_results)) {
   cat("\\hline\n")
   cat("Model & Alpha & TestVersion & Significance & SymbolsTested & Passed & Failed \\\\\n")
   cat("\\hline\n")
-  
+
   df <- esr_results[[key]]
   df$Alpha <- factor(df$Alpha, levels = c(0.01, 0.025, 0.05), labels = c("1%", "2.5%", "5%"))
-  
+
   for (i in 1:nrow(df)) {
     row <- df[i, ]
     cat(paste(row$Model, "&", row$Alpha, "&", row$TestVersion, "&", row$Significance, "&", row$SymbolsTested, "&", row$Passed, "&", row$Failed, "\\\\\n"))
     cat("\\hline\n")
   }
-  
+
   cat("\\end{tabular}\n")
   cat("\\end{table}\n")
 }
@@ -445,70 +449,72 @@ for (key in names(esr_results)) {
 ###########################################################
 # isolate one model to see what happens
 # pick your model & return data
-model_df   <- transformer_RV_IV_ensemble_rolling
-return_df  <- return_data
+model_df <- transformer_RV_IV_ensemble_rolling
+return_df <- return_data
 
-symbols    <- unique(model_df$Symbol)
-alphas     <- alpha_config_lstm_transformer$levels
+symbols <- unique(model_df$Symbol)
+alphas <- alpha_config_lstm_transformer$levels
 
 # prepare a list to collect everything
 results_list <- list()
 
 for (alpha in alphas) {
-  alpha_col <- alpha_config_lstm_transformer$columns[[ as.character(alpha) ]]
-  es_col    <- es_config_lstm_transformer$columns   [[ as.character(alpha)  ]]
-  
+  alpha_col <- alpha_config_lstm_transformer$columns[[as.character(alpha)]]
+  es_col <- es_config_lstm_transformer$columns[[as.character(alpha)]]
+
   cat("\n=== ?? =", alpha, "(", alpha_col, "/", es_col, ") ===\n")
-  
+
   for (sym in symbols) {
     cat("  ???", sym, "??? ")
-    
+
     # grab + align
     returns <- return_df %>%
       filter(Symbol == sym) %>%
       select(Date, LogReturn) %>%
       arrange(Date)
-    
+
     preds <- model_df %>%
       filter(Symbol == sym) %>%
       select(Date, all_of(c(alpha_col, es_col))) %>%
       arrange(Date)
-    
+
     joint <- inner_join(returns, preds, by = "Date")
     if (nrow(joint) == 0) {
       cat("no dates ??? skip\n")
       next
     }
-    
+
     # run ESR
     res <- tryCatch(
       esr_backtest(
-        r     = joint$LogReturn,
-        q     = joint[[alpha_col]],
-        e     = joint[[es_col]],
+        r = joint$LogReturn,
+        q = joint[[alpha_col]],
+        e = joint[[es_col]],
         alpha = alpha,
         version = 1,
-        cov_config = list(sparsity="nid", sigma_est="scl_sp", misspec=TRUE)
+        cov_config = list(sparsity = "nid", sigma_est = "scl_sp", misspec = TRUE)
       ),
       error = function(e) NULL
     )
-    
+
     if (is.null(res) || is.null(res$pvalue_twosided_asymptotic)) {
       cat("error/NA p-val\n")
       next
     }
-    
-    pval   <- res$pvalue_twosided_asymptotic
+
+    pval <- res$pvalue_twosided_asymptotic
     passed <- pval >= 0.05
-    cat("p-val=", round(pval, 3),
-        ifelse(passed, "[PASS]\n", "[FAIL]\n"))
-    
+    cat(
+      "p-val=", round(pval, 3),
+      ifelse(passed, "[PASS]\n", "[FAIL]\n")
+    )
+
     # store
-    results_list[[ length(results_list) + 1 ]] <- data.frame(
-      Alpha   = alpha,
-      Symbol  = sym,
+    results_list[[length(results_list) + 1]] <- data.frame(
+      Alpha = alpha,
+      Symbol = sym,
       p_value = pval,
-      Passed  = passed,
+      Passed = passed,
       stringsAsFactors = FALSE
     )
   }
@@ -517,5 +523,4 @@ for (alpha in alphas) {
 # combine & display
 library(knitr)
 all_sym_results <- do.call(rbind, results_list)
-kable(all_sym_results, digits=3, caption="ESR p-values")
-
+kable(all_sym_results, digits = 3, caption = "ESR p-values")
